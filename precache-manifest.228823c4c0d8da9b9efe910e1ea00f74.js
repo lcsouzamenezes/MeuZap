d09b32bdca971c9378e408a3161c3315d8da4cd1
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-06-18 21:59:49 -0300

    Updates

---
+++ precache-manifest.228823c4c0d8da9b9efe910e1ea00f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d0694fa8b6fce874bf272b45ced0a9",
+    "url": "/gerador-link-whatsapp/index.html"
+  },
+  {
+    "revision": "e74768586fe8dd4a704e",
+    "url": "/gerador-link-whatsapp/static/css/2.03923625.chunk.css"
+  },
+  {
+    "revision": "7138afb78fa60ce08ed6",
+    "url": "/gerador-link-whatsapp/static/css/main.fe9cfbe7.chunk.css"
+  },
+  {
+    "revision": "e74768586fe8dd4a704e",
+    "url": "/gerador-link-whatsapp/static/js/2.6fd2ac77.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gerador-link-whatsapp/static/js/2.6fd2ac77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7138afb78fa60ce08ed6",
+    "url": "/gerador-link-whatsapp/static/js/main.e2d38cac.chunk.js"
+  },
+  {
+    "revision": "77e44392a94e0429d4db",
+    "url": "/gerador-link-whatsapp/static/js/runtime-main.e3749a0a.js"
+  }
+]);
